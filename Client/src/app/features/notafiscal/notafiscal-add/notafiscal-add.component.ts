b1d40a3ec84d7b93cbf74ab0b41c1d8464ee4737
Caroline Antonello <caroline_antonello@hotmail.com>
2018-10-15 16:51:03 -0300

    alteraçõe em add de nota fiscal

--- Client/src/app/features/notafiscal/notafiscal-add/notafiscal-add.component.ts
+++ Client/src/app/features/notafiscal/notafiscal-add/notafiscal-add.component.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NotaFiscalService } from '../shared/notafiscal.service';
 import { Router } from '@angular/router';
+import { NotaFiscal, NotaFiscalDataCommand } from '../shared/notafiscal.model';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
     templateUrl: './notafiscal-add.component.html',
 })
 export class NotaFiscalAddComponent implements OnInit {
     public title: string = 'Nova Nota Fiscal';
+    public isLoading: boolean;
+    public notafiscal: NotaFiscal;
+    public data: any[];
+    public delay: number = 300;
     public form: FormGroup = this.fb.group({
         naturezaOperacao: ['', Validators.required],
         dataEntrada: ['', [Validators.required]],
@@ -25,4 +31,26 @@ export class NotaFiscalAddComponent implements OnInit {
     public ngOnInit(): void {
         //
      }
+     public onAutoCompleteChange(value: string): any {
+        Observable.of(value)
+          .delay(this.delay)
+          .switchMap((value: any, index: number) => this.service.getByName(value))
+          .subscribe((response: any) => {
+            this.data = response;
+          });
+      }
+    public onSave(): void {
+        this.isLoading = true;
+        const notaFiscalAddCommand: NotaFiscalDataCommand = new NotaFiscalDataCommand(this.form.value);
+
+        this.service.add(notaFiscalAddCommand)
+        .take(1)
+        .subscribe(() => {
+            this.redirect();
+            this.isLoading = false;
+        });
+    }
+    public redirect(): void {
+        this.router.navigate(['./vendas']);
+    }
 }
